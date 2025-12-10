6478c5a27f3b26347e80590d588f99a2a68fa9b4
Vishal Patil <89679996+vishalp743@users.noreply.github.com>
2025-12-10 15:40:30 +0530

    Update loginLimiter.js

--- server/middleware/loginLimiter.js
+++ server/middleware/loginLimiter.js
@@ -3,7 +3,7 @@ const rateLimit = require('express-rate-limit');
 
 const loginLimiter = rateLimit({
     windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 5, // Limit each IP to 5 login requests per windowMs
+    max: 50, // Limit each IP to 5 login requests per windowMs
     message: {
         msg: 'Too many login attempts from this IP, please try again after 15 minutes'
     },
